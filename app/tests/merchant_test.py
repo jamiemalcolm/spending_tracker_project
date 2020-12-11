f1b1024ad8c7fcc16a3761473d13e2c8fdd81fe6
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-11 14:24:42 +0000

    first merchant test passed

---
+++ app/tests/merchant_test.py
@@ -0,0 +1,10 @@
+import unittest
+from app.models.merchant import Merchant
+
+class TestMerchant(unittest.TestCase):
+
+    def setUp(self):
+        self.merchant = Merchant("Sainsbury's")
+
+    def test_merchant_has_name(self):
+        self.assertEqual("Sainsbury's", self.merchant.name)
---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+from app.tests.merchant_test import TestMerchant
+
+if __name__ == '__main__':
+    unittest.main()
