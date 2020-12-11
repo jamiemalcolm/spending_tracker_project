5bb7fb8cf6f8973b93a5e9277c6cd397aa3998f1
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-11 18:26:44 +0000

    tag_repository written and tested

--- app/console.py
+++ app/console.py
@@ -4,6 +4,8 @@ from models.tag import Tag
 from models.transaction import Transaction
 
 import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
+
 
 merchant_1 = Merchant("Sainsburys")
 merchant_repository.save(merchant_1)
@@ -12,12 +14,7 @@ merchant_2 = Merchant("Tesco")
 merchant_repository.save(merchant_2)
 
 
-all_merchants = merchant_repository.select_all()
-
-print(all_merchants)
-
-sainsburys = merchant_repository.select(1)
-
-print(sainsburys)
-
-merchant_repository.delete_all()
+tag_1 = Tag("Grocories")
+tag_repository.save(tag_1)
+tag_2 = Tag("Bills")
+tag_repository.save(tag_2)
--- app/repositories/tag_repository.py
+++ app/repositories/tag_repository.py
@@ -7,13 +7,47 @@ from models.transaction import Transaction
 # write sql functions
 
 # save 
+def save(tag):
+    sql = "INSERT INTO tags (category, active) VALUES (%s, %s) RETURNING *"
+    values = [tag.category, tag.active]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    tag.id = id
+    return tag
 
 # select all 
+def select_all():
+    tags = []
 
+    sql = "SELECT * FROM tags"
+    results = run_sql(sql)
+
+    for row in results:
+        tag = Tag(row['category'], row['active'], row['id'])
+        tags.append(tag)
+    return tags
 # select by id 
+def select(id):
+    tag = None
 
+    sql = "SELECT * FROM tags WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)
+
+    if result is not None:
+        tag = Tag(result['category'], result['active'], result['id'])
+    return tag
 # delete all 
-
+def delete_all():
+    sql = "DELETE FROM tags"
+    run_sql(sql)
 #delete by id 
-
+def delete(id):
+    sql = "DELETE FROM tags WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 # update 
+def update(tag):
+    sql = "UPDATE tags SET(category, active) = (%s, %s) WHERE id = %s"
+    values = [tag.category, tag.active, tag.id]
+    run_sql(sql, values)
