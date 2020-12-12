57b3908adb83a0b79305af9739c176439da1d8bc
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-12 11:59:27 +0000

    templates and controllers created

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from controllers.transaction_controller import transaction_blueprint
+from controllers.tag_controller import tag_blueprint
+from controllers.merchant_controller import merchant_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(transaction_blueprint)
+app.register_blueprint(tag_blueprint)
+app.register_blueprint(merchant_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ =='__main__':
+    app.run(debug=True)
