ec8326d31c067568aac7a44cf29d9a02304943a3
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-14 10:01:14 +0000

    submit button on merchant adding to data base

--- app/controllers/merchant_controller.py
+++ app/controllers/merchant_controller.py
@@ -14,12 +14,12 @@ def merchants():
 def new_merchant():
     return render_template("/merchants/new.html", title ="New Merchant")
 
-@merchants_blueprint.route("/merchants/new", methods =["POST"], ["GET"])
+@merchants_blueprint.route("/merchants/new", methods =["POST"])
 def create_new_merchant():
     name = request.form['name']
 
     merchant = Merchant(name)
     merchant_repository.save(merchant)
 
-    return render_template("/merchants/index.html", merchants = merchants)
+    return render_template("/merchants/new.html", merchants = merchants)
 
