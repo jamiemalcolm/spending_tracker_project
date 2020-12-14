d35affc5e5dbbef676096768cf038a3dddcfdc56
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-14 09:49:25 +0000

    form submit written for merchant

--- app/controllers/merchant_controller.py
+++ app/controllers/merchant_controller.py
@@ -10,5 +10,16 @@ def merchants():
     merchants = merchant_repository.select_all()
     return render_template("/merchants/index.html", merchants = merchants)
 
+@merchants_blueprint.route("/merchants/new")
+def new_merchant():
+    return render_template("/merchants/new.html", title ="New Merchant")
 
+@merchants_blueprint.route("/merchants/new", methods =["POST"], ["GET"])
+def create_new_merchant():
+    name = request.form['name']
+
+    merchant = Merchant(name)
+    merchant_repository.save(merchant)
+
+    return render_template("/merchants/index.html", merchants = merchants)
 
