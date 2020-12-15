2fce744aea2fe08d583bd52349d989a0cd1b3706
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-15 10:22:45 +0000

    timestamp added to transaction table, app still running

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -1,13 +1,10 @@
 class Transaction:
 
-    def __init__(self, merchant, tag, amount, id = None):
+    def __init__(self, merchant, tag, amount, id = None, time = None):
         self.merchant = merchant
         self.tag = tag
         self.amount = amount
         self.id = id
+        self.time = time
 
     
-    
-    # def convert_to_pennies(self, amount):
-    #     before = int(amount * 100)
-    #     return before        
